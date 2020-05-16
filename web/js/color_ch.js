d3aa3d575b378592e920254f6287fafd02b542bb
LiXiong_win <li.xiong.terance@gmail.com>
2020-05-16 22:47:29 +0800

    增加：1. 返回图片下载 2. 转换按钮状态显示 3.添加 index.html

--- web/js/color_ch.js
+++ web/js/color_ch.js
@@ -9,24 +9,56 @@ function change_img() {
 }
 
 function refresh_code() {
-    var url = '/api/tools/code/' + '?t=' + (new Date()).getTime();
+    url = '/api/tools/code/' + '?t=' + (new Date()).getTime();
     $('#img_code').attr('src', url)
 }
 
 function upload() {
-    var payload = new FormData($('#form1')[0]);
-    $.ajax({
+    payload = new FormData($('#form1')[0]);
+    $('#button_ch').attr('style', 'display: none');
+    $('#button_ching').removeAttr('style');
+    upload_ajax = $.ajax({
         url: '/api/colorch/upload/',
         type: 'POST',
+        data: payload,
+        timeout : 60000, //超时时间设置，单位毫秒
         processData: false,
         contentType: false,
-        data: payload,
         success: function(data){
-                    if ('success' == data['status']) {
-                        $('#img_reponse').attr('src', 'data:image/jpg;base64,' + data['data']);
-                    } else {
-                        alert(data['msg']);
-                    }
-                }
+            if ('success' == data['status']) {
+                $('#img_response').attr('src', 'data:image/jpg;base64,' + data['data']);
+                $('#button_ch').removeAttr('style');
+                $('#button_ching').attr('style', 'display: none');
+                refresh_code();
+            } else {
+                $('#button_ch').removeAttr('style');
+                $('#button_ching').attr('style', 'display: none');
+                alert(data['msg']);
+            }
+        },
+        error: function(){
+            alert('请求出错，请重试！')
+            $('#button_ch').removeAttr('style');
+            $('#button_ching').attr('style', 'display: none');
+            refresh_code();
+        },
+        complete: function(XMLHttpRequest, status){
+            if('timeout' == status){
+                upload_ajax.abort();
+                alert('请求超时，请重试！')
+                $('#button_ch').removeAttr('style');
+                $('#button_ching').attr('style', 'display: none');
+                refresh_code();
+    　　　　}
+    　　}
     });
+}
+
+function download() {
+    url = $('#img_response').attr('src');
+    tmp_a = $('<a id="tmp_a" download="pic.png" style="display:none"></a>');
+    $('#img_response').append(tmp_a);
+    $('#tmp_a').attr('href', url);
+    $('#tmp_a')[0].click();
+    $('#tmp_a').remove();
 }
